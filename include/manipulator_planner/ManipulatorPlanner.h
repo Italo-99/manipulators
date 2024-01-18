22b071cba013d85d6471bbd13e3e554aaf555796
Italo <255778@studenti.unimore.it>
2024-01-18 16:10:12 +0100

    Completed manipulator planner class

--- include/manipulator_planner/ManipulatorPlanner.h
+++ include/manipulator_planner/ManipulatorPlanner.h
@@ -1,38 +1,100 @@
+/*
+ * Software License Agreement (Apache Licence 2.0)
+ *
+ *  Copyright (c) [2024], [Andrea Pupa] [italo Almirante]
+ *  All rights reserved.
+ *
+ *  Redistribution and use in source and binary forms, with or without
+ *  modification, are permitted provided that the following conditions
+ *  are met:
+ *
+ *   1. Redistributions of source code must retain the above copyright
+ *      notice, this list of conditions and the following disclaimer.
+ *   2. Redistributions in binary form must reproduce the above copyright
+ *      notice, this list of conditions and the following disclaimer in
+ *      the documentation and/or other materials provided with the
+ *      distribution.
+ *   3. The name of the author may not be used to endorse or promote
+ *      products derived from this software without specific prior
+ *      written permission.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ *  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ *  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ *  FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ *  COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ *  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ *  BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ *  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ *  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ *  LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ *  ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ *  POSSIBILITY OF SUCH DAMAGE.
+ *
+ *  Author: [Andrea Pupa] [Italo Almirante]
+ *  Created on: [2024-01-17]
+ */
+
+// CLASS HEADER IMLEMENTATION OF MANIPULATOR PLANNER -> CHILD OF DYNAMIC PLANNER 
+
 #ifndef MANIPULATOR_PLANNER_H
 #define MANIPULATOR_PLANNER_H
 
 // IMPORT LIBRARIES
 #include "dynamic_planner/dynamic_planner.h"
-#include "ros/ros.h"
 
-// CLASS OF MANIPULATOR PLANNER, THAT USES FUNCTIONS OF THE DYNAMIC PLANNER
 class ManipulatorPlanner
 {
-
-  // Public functions declarations
  public:
-  ManipulatorPlanner();     // Constructor
-  ~ManipulatorPlanner();    // Destructor
-  void spinner(void);       // Asynchronous spinner for ROS routines
+  // ---------------------  PUBLIC CONSTRUCTOR ---------------------
+      ManipulatorPlanner();     // Constructor
+      ~ManipulatorPlanner();    // Destructor
 
-// Private functions declarations
+  // ---------------------  PUBLIC FUNCTIONS ---------------------
+
+    void spinner(void);       // Asynchronous spinner for ROS routines
+
+    // Create a collision object from a selected primitive
+    moveit_msgs::CollisionObject createObj(std::string& name, 
+                                         uint8        obj_type, 
+                                         float64      obj_dims, 
+                                         float64      obj_pos, 
+                                         bool         rot_90);
+
  private:
 
-  // Callback function for goals in the 3D cartesian space for the TCP
-  void tcpGoalCallback(const geometry_msgs::Pose::ConstPtr& p);
+  // --------------------- PRIVATE FUNCTIONS ---------------------
+    // --------------------- UTILS FUNCTIONS ---------------------
 
-  // Callback function for goals in the joint space
-  void jointsGoalCallback(const sensor_msgs::JointState::ConstPtr& js);
+      void check_param(std::string manipulator_name,
+                          double vel_factor, 
+                          double acc_factor,
+                            bool sim);
 
-  // Private class variables declaration
-  ros::NodeHandle nh_;                    // Node object
-  ros::Subscriber tcp_goal_sub_;          // Subscriber to TCP goal
-  ros::Subscriber joint_goal_sub_;        // Publisher to joint goal
+    // --------------------- MOVE FUNCTIONS ---------------------
 
-  std::vector<std::string> joint_names_;  // Joints' names
-  std::string ee_name_;                   // End-effector's name
-  std::string base_name_;                 // Robot base's name
-  DynamicPlanner* planner_;               // Dynamic planner object 
+      // Callback function for goals in the 3D cartesian space for the robot TCP
+      void tcpGoalCallback(const geometry_msgs::Pose::ConstPtr& p);
+
+      // Callback function for goals in the joint space
+      void jointsGoalCallback(const sensor_msgs::JointState::ConstPtr& js);
+
+      // Callback function for goals in the 3D cartesian space for the robot TCP
+      void tcpGoalSeqCallback(std::vector<const geometry_msgs::Pose::ConstPtr>& p_seq);
+
+      // Callback function for goals in the joint space
+      void jointsGoalSeqCallback(std::vector<const sensor_msgs::JointState::ConstPtr>& js_seq);
+
+  // ---------------------  PRIVATE VARIABLES ---------------------
+
+    ros::NodeHandle nh_;                    // Node object
+    ros::Subscriber tcp_goal_sub_;          // Subscriber to TCP goal
+    ros::Subscriber joint_goal_sub_;        // Publisher to joint goal
+
+    std::vector<std::string> joint_names_;  // Joints' names
+    std::string ee_name_;                   // End-effector's name
+    std::string base_name_;                 // Robot base's name
+    DynamicPlanner* planner_;               // Dynamic planner object 
 };
 
 #endif /* MANIPULATOR_PLANNER_H */
--- src/manipulator_planner.cpp
+++ src/manipulator_planner.cpp
@@ -1,6 +1,44 @@
-// THE FOLLOWING CODE INTRODUCES THE CLASS OF MANIPULATOR PLANNER
-// WHICH INCLUDES AN INSTANCE OF DYNAMIC PLANNER
-// AND A "TABLE" OBJECT IN RVIZ TO TEST PLANNING
+/*
+ * Software License Agreement (Apache Licence 2.0)
+ *
+ *  Copyright (c) [2024], [Andrea Pupa] [italo Almirante]
+ *  All rights reserved.
+ *
+ *  Redistribution and use in source and binary forms, with or without
+ *  modification, are permitted provided that the following conditions
+ *  are met:
+ *
+ *   1. Redistributions of source code must retain the above copyright
+ *      notice, this list of conditions and the following disclaimer.
+ *   2. Redistributions in binary form must reproduce the above copyright
+ *      notice, this list of conditions and the following disclaimer in
+ *      the documentation and/or other materials provided with the
+ *      distribution.
+ *   3. The name of the author may not be used to endorse or promote
+ *      products derived from this software without specific prior
+ *      written permission.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ *  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ *  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ *  FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ *  COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ *  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ *  BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ *  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ *  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ *  LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ *  ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ *  POSSIBILITY OF SUCH DAMAGE.
+ *
+ *  Author: [Andrea Pupa] [Italo Almirante]
+ *  Created on: [2024-01-17]
+ */
+
+// CLASS SOURCE IMLEMENTATION OF MANIPULATOR PLANNER -> CHILD OF DYNAMIC PLANNER 
+
+// The following code introduces the class of manipulator planner, an instance of the
+// dynamic planner, plus a table object in RViz to test planning
 
 // IMPORT LIBRARIES
 #include "manipulator_planner/ManipulatorPlanner.h"
@@ -8,57 +46,156 @@
 // TODO: together with node launch, UR10 should go immediatly to a preconfigured configuration
 // initial position {0,-90,+90,-90,-90,0}
 
-// MANIPULATOR PLANNER CLASS
+
+// ---------------------  PUBLIC CONSTRUCTOR ---------------------
+
 ManipulatorPlanner::ManipulatorPlanner()
 {
-  // TCP goal subscriber
-  tcp_goal_sub_ =
-    nh_.subscribe("/desired_tcp_pose", 1, &ManipulatorPlanner::tcpGoalCallback, this);
-  
-  // Joint goal subscriber
-  joint_goal_sub_ = nh_.subscribe("/desired_joint_pose", 1,
-                                  &ManipulatorPlanner::jointsGoalCallback, this);
+  // ---------------------  TCP AND JOINT GOALS SUBSCRIBERS ---------------------
 
-  // Class variables declarations .> SHOULD THEY BE PUT INTO THE HEADER FILE??????
+  tcp_goal_sub_   = nh_.subscribe("/desired_tcp_pose",   1, &ManipulatorPlanner::tcpGoalCallback,    this);
+  joint_goal_sub_ = nh_.subscribe("/desired_joint_pose", 1, &ManipulatorPlanner::jointsGoalCallback, this);
+
+  tcp_goal_sub_   = nh_.subscribe("/desired_tcpSeq_poses",   1, &ManipulatorPlanner::tcpGoalSeqCallback,    this);
+  joint_goal_sub_ = nh_.subscribe("/desired_jointSeq_poses", 1, &ManipulatorPlanner::jointsGoalSeqCallback, this);
+
+
+  // Planner args
   std::string manipulator_name;     // Manipulator name
   double vel_factor, acc_factor;    // Scale factor for joint velocities and accelerations
   bool sim;                         // Simulation status (true for sim, false for debug)
 
-  // This variable checks if something is missing among simulation parameters
-  // BUT WHAT IF THIS MANIPULATOR IS LAUNCHED FROM A NODE???
-  bool params = true;
+  // CALL TO THE DYNAMIC PLANNER
+  planner_ = new DynamicPlanner(manipulator_name, joint_names_, vel_factor, acc_factor, false);
+
+  // Set the sim mode for the dynamic planner
+  planner_->setSimMode(sim);
+
+  // Add a table to the scene
+  moveit_msgs::CollisionObject table = createObj("table", 1, [4,4,0.079], [0,0,-0.04]);  
+
+  // Update collision objects scene
+  planner_->getPlanningSceneInterface().applyCollisionObjects(planner_->getCollisionObjects());
+}
+
+// Destructor of the object manipulator planner's class
+ManipulatorPlanner::~ManipulatorPlanner() {delete planner_;}
+
+// ---------------------  PUBLIC FUNCTIONS ---------------------
+
+// Manipulator planner spin function -> NOTE: the sleep rate is set in the node
+void ManipulatorPlanner::spinner()  {ros::spinOnce();}
+
+// Creation of a collision object
+moveit_msgs::CollisionObject ManipulatorPlanner::createObj(std::string& name, 
+                                                           uint8        obj_type, 
+                                                           float64      obj_dims, 
+                                                           float64      obj_pos = [0,0,0], 
+                                                           bool         rot_90  = false)
+{
+  // Creation of the obj
+  moveit_msgs::CollisionObject obj;
+
+  obj.header.frame_id = base_name_;
+  obj.id              = name;
+  obj.primitives[0].type = obj_type;
+  obj.operation = 0;  // static obj
+  obj.primitives[0].dimensions.resize(uint8(size(obj_dims)));
+
+  // Set primitive type
+  switch(obj_type)
+  {
+    case 1:   // BOX: Rectangular shape setting
+      if (size(obj_dims) != 3) {ROS_WARN_THROTTLE(3, "obj_dims array is not compatible with obj_type");}
+      else                     {obj.primitives.resize(1);
+                                // Set the three dimensions of the parallelepiped
+                                obj.primitives[0].dimensions[0] = obj_dims[0];
+                                obj.primitives[0].dimensions[1] = obj_dims[1];
+                                obj.primitives[0].dimensions[2] = obj_dims[2];}
+      break;
+
+    case 2:   // SPHERE
+      if (size(obj_dims) != 1) {ROS_WARN_THROTTLE(3, "obj_dims array is not compatible with obj_type");}
+      else                     {obj.primitives.resize(1);
+                                // Set the sphere radius
+                                obj.primitives[0].dimensions[0] = obj_dims[0];}
+      break;
+
+    default:   // CYLINDER OR CONE
+      if (size(obj_dims) != 2) {ROS_WARN_THROTTLE(3, "obj_dims array is not compatible with obj_type");}
+      else                     {obj.primitives.resize(1);
+                                // Set height and radius of the cylinder/cone
+                                obj.primitives[0].dimensions[0] = obj_dims[0];
+                                obj.primitives[0].dimensions[1] = obj_dims[1];}
+      break;
+  }
+
+  // Set obj position
+  obj.primitive_poses.resize(1);
+  obj.primitive_poses[0].position.x = obj_pos[0];
+  obj.primitive_poses[0].position.y = obj_pos[1];
+  obj.primitive_poses[0].position.z = obj_pos[2];
+
+  // Set obj orientation
+  if (rot90)
+  {
+    obj.primitive_poses[0].orientation.x = 0;
+    obj.primitive_poses[0].orientation.y = 0;
+    obj.primitive_poses[0].orientation.z = PI/4;
+    obj.primitive_poses[0].orientation.w = PI/4;
+  }
+  else
+  {
+    obj.primitive_poses[0].orientation.x = 0;
+    obj.primitive_poses[0].orientation.y = 0;
+    obj.primitive_poses[0].orientation.z = 0;
+    obj.primitive_poses[0].orientation.w = 1;
+  }
+
+
+  // THIS IS THE WAY TO HANDLE OBSTACLES. Push back in the vector if you want to add, 
+  // remove from the vector if you want to remove. Be sure to also process and apply!
+  planner_->getCollisionObjects().push_back(obj);                       // add the obj object as obstacle
+  planner_->getPlanningScenePtr()->processCollisionObjectMsg(obj);      // map the collision object into the joint space
+
+  return obj;
+}
+
+// Check manipulators parameters passed to the node
+void ManipulatorPlanner::check_param(std::string manipulator_name,
+                                     double      vel_factor, 
+                                     double      acc_factor);
+{
+  // If one of the following parameters has not been defined, shutdwon ROS
 
   // Check for manipulator name parameter
   if (!nh_.getParam("manipulator_planner/manipulator_name", manipulator_name))
   {
     ROS_ERROR("Manipulator name not defined");
-    params = false;
+    ros::shutdown();
+    return;
   }
 
   // Check for joint names parameter
   if (!nh_.getParam("manipulator_planner/joint_names", joint_names_))
   {
     ROS_ERROR("Joint names not defined");
-    params = false;
+    ros::shutdown();
+    return;
   }
 
   // Check for ee name parameter
   if (!nh_.getParam("manipulator_planner/ee_name", ee_name_))
   {
     ROS_ERROR("End-effector name not defined");
-    params = false;
+    ros::shutdown();
+    return;
   }
 
   // Check for robot base name parameter
   if (!nh_.getParam("manipulator_planner/base_name", base_name_))
   {
     ROS_ERROR("Base name not defined");
-    params = false;
-  }
-
-  // If one of the parameters above has not been set, shutdown the roscore
-  if (!params)
-  {
     ros::shutdown();
     return;
   }
@@ -81,63 +218,11 @@ ManipulatorPlanner::ManipulatorPlanner()
     acc_factor = 0.5;
   }
 
-  // Get simulation goal status from the user (simulation or debug)
+  // Get simulation status from the user (simulation or debug)
   nh_.getParam("manipulator_planner/sim", sim);
-
-
-  // CREATION OF A TABLE AS COLLISION OBJECT
-  moveit_msgs::CollisionObject table;
-
-  // Set the position of the table relating to the robot base frame
-  table.header.frame_id = base_name_;
-  table.id              = "table";
-
-  // Rectangular shape setting
-  table.primitives.resize(1);
-  table.primitives[0].type = 1;
-
-  // Set the three dimensions of the parallelepaid
-  table.primitives[0].dimensions.resize(3);
-  table.primitives[0].dimensions[0] = 4;
-  table.primitives[0].dimensions[1] = 4;
-  table.primitives[0].dimensions[2] = 0.079;
-
-  // Set table position
-  table.primitive_poses.resize(1);
-  table.primitive_poses[0].position.x = 0;
-  table.primitive_poses[0].position.y = 0;
-  table.primitive_poses[0].position.z = -0.04;
-
-  // Set table orientation
-  table.primitive_poses[0].orientation.x = 0;
-  table.primitive_poses[0].orientation.y = 0;
-  table.primitive_poses[0].orientation.z = 0;
-  table.primitive_poses[0].orientation.w = 1;
-
-  // Set the table static
-  table.operation = 0;
-
-  // CALL TO THE DYNAMIC PLANNER
-  planner_ = new DynamicPlanner(manipulator_name, joint_names_, vel_factor, acc_factor);
-
-  // Set the sim mode for the dynamic planner
-  planner_->setSimMode(sim);
-
-  // THIS IS THE WAY TO HANDLE OBSTACLES. Push back in the vector if you want to add,
-  // remove from the vector if you want to remove. You must add a callback that reads data
-  // from optitrack and push back or modify the objects. Be sure to also process and
-  // apply!
-
-  planner_->getCollisionObjects().push_back(table);                       // add the table object as obstacle
-  planner_->getPlanningScenePtr()->processCollisionObjectMsg(table);      // map the collision object into the joint space
-  planner_->getPlanningSceneInterface().applyCollisionObjects(            // apply the collision object to the planning scene interface
-            planner_->getCollisionObjects());
 }
 
-// Destructor of the object manipulator planner's class
-ManipulatorPlanner::~ManipulatorPlanner() { delete planner_; }
-
-// CALLBACK FUNCTION TO HANDLE THE TCP GOAL
+// Callback function to handle a tcp 3D goal
 void ManipulatorPlanner::tcpGoalCallback(const geometry_msgs::Pose::ConstPtr& p)
 {
 
@@ -165,7 +250,7 @@ void ManipulatorPlanner::tcpGoalCallback(const geometry_msgs::Pose::ConstPtr& p)
   planner_->plan(goal, ee_name_);
 }
 
-// CALLBACK FUNCTION TO HANDLE THE JOINT GOAL
+// Callback function to handle a joint goal
 void ManipulatorPlanner::jointsGoalCallback(const sensor_msgs::JointState::ConstPtr& js)
 {
   // Verify if the joint name vector size is the same as the joint goal passed from the publisher
@@ -179,13 +264,55 @@ void ManipulatorPlanner::jointsGoalCallback(const sensor_msgs::JointState::Const
   planner_->plan(js->position);
 }
 
-// Manipulator planner spin function
-void ManipulatorPlanner::spinner()
+// Callback function for goals in the 3D cartesian space for the robot TCP
+void tcpGoalSeqCallback(std::vector<const geometry_msgs::Pose::ConstPtr>& p_seq);
+{ 
+  // Create a vector of PoseStamped msgs
+  std::vector<const geometry_msgs::PoseStamped> p_stamp_seq;
+  
+  // Convert each pose of the input vector into PoseStamped goals
+  for (const auto& p : p_stamp_seq)
+  {   
+    // Declaration of the goal variable as PS
+    geometry_msgs::PoseStamped goal;
+
+    // Fill the fields of the goal variable
+    goal.header.frame_id = base_name_;
+    // Set the pose as passed from the publisher
+    goal.pose            = *p;                              
+
+    // Check if the quaternion has unit norm, if not return an error
+    tf2::Quaternion quat_tf;
+    tf2::convert(goal.pose.orientation, quat_tf);
+    if (quat_tf.length() >= 1.1 || quat_tf.length() <= 0.9)
+    {
+      ROS_ERROR("Quaternion must have unit norm.");
+      return;
+    }
+    // If the norm is not so far from the unit, normalize the orientation quaternion
+    quat_tf.normalize();
+    goal.pose.orientation = tf2::toMsg(quat_tf);
+
+    // Push back the goal
+    p_stamp_seq.push_back(goal);
+  }
+
+  // Call the sequencial planner
+  plan(p_seq, ee_name_); 
+}
+
+// Callback function for goals in the joint space
+void jointsGoalSeqCallback(std::vector<const sensor_msgs::JointState::ConstPtr>& js_seq)
 {
-  // Simple ROS once spinner (NOTE: the sleep rate is set outside of this class implementation)
-  ros::spinOnce();
-  // The scaling MUST publish on the topic /trajectory_counter!!!
-  planner_->checkTrajectory();  // UPDATE THE TRAJECTORY GOAL STATE OF THE DYNAMIC PLANNER
-                                // FIND THIS FUNCTION IN THE DYNAMIC PLANNER CLASS TO UNDERSTAND !!!!!!!!!!!!
-  return;
+  // Create a vector of vectors
+  std::vector<std::vector<int>> js_seq_vecs;
+
+  // Convert each joint state msg into a vector
+  for (const auto& js : js_seq)
+  {
+    js_seq_vecs.push_back(js.position);
+  }
+
+  // Call the sequential planner
+  plan(js_seq_vecs);
 }
