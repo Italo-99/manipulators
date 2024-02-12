a36a38a5623928975dd918881e1f7c044729a518
Italo <255778@studenti.unimore.it>
2024-02-12 19:59:43 +0100

    Group ur10+camera+gripper perfectly working

--- src/manipulator_menu.cpp
+++ src/manipulator_menu.cpp
@@ -201,7 +201,7 @@ void ManipulatorMenu::goHome(const bool ee_orient)
 {
   std::vector<double> start_joint_pose = {0.,0.,0.,0.,0.,0};
   if (!ee_orient) // gripper down
-  {start_joint_pose = {0.,-90.,+90.,-90.,-90.,0};}
+  {start_joint_pose = {0.,-90.,+90.,-90.,-90.,180};}
   else // gripper at the front
   {start_joint_pose = {0.,-90.,+90.,  0.,+90.,0};}
   // Publishe home joint goal 
