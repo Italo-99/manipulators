5569fbbb533b6ccc1f86b396809fd6dc057fa230
gLippoli <giannilippoli@hotmail.it>
2025-07-31 19:56:17 +0200

    modifiche

--- launch/planning_context.launch.py
+++ launch/planning_context.launch.py
@@ -75,7 +75,7 @@ def launch_setup_ur(context, *args, **kwargs):
         output="screen",
         namespace=get_namespace(context),
         parameters=moveit_params,
-        arguments=['--ros-args', '--log-level', 'moveit_core.constraint_samplers:=debug', '--log-level', 'ompl:=debug'],
+        arguments=[],
             # robot_description,
             # robot_description_semantic,
             # robot_description_kinematics,
